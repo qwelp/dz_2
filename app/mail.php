7e3c361583a788a6e9f92aecf7f9f0ae4be9fb0d
qwelp <qwelp@mail.ru>
2015-08-19 15:22:24 +0300

    Добавлени mail.php

--- app/mail.php
+++ app/mail.php
@@ -1,3 +1,30 @@
-<?
-print_r($_POST);
+﻿<?php
+
+header('Content-Type: application/json');
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$msg = $_POST['msg'];
+$day = $_POST['day'];
+$hours = $_POST['hours'];
+
+//действия с данными
+
+$message = '';
+$message .= 'Имя: '.$name."\r\n";
+$message .= 'Телефон: '.$phone."\r\n";
+$message .= 'Комментарий: '.$msg."\r\n";
+$message .= 'Время : '.$day.' часа : '.$hours.' минут';
+
+sleep(1);
+
+$result = true;
+
+mail('qwelp@mail.ru', 'Заказан звонок', $message);
+
+echo json_encode(array(
+	'status' => $result,
+	'dump' => $_POST
+));
+
 ?>
